128d15d1d84d1c6eea8751cb066681d63375f4ff
OceanOC <141111946+OceanOC@users.noreply.github.com>
2024-08-21 10:41:16 +0530

    Switch to Command Prompt for unzip
    Replace Powershell's Expand-Archive with Command Prompt's tar to fix #2

--- json.go
+++ json.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-  "os"
-  "encoding/json"
-  "io"
+	"encoding/json"
+	"io"
+	"os"
 )
 
-func openjson(tmpfolder string) map[string]interface{}  {
-  jsonFile, err := os.Open(tmpfolder + "")
-  if err != nil {
-    panic(err)
-  }
-  byteValue, _ := io.ReadAll(jsonFile)
+func openjson(tmpfolder string) map[string]interface{} {
+	jsonFile, err := os.Open(tmpfolder + "")
+	if err != nil {
+		panic(err)
+	}
+	byteValue, _ := io.ReadAll(jsonFile)
 
-  var result map[string]interface{}
-  json.Unmarshal([]byte(byteValue), &result)
-  defer jsonFile.Close()
-  return result
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+	defer jsonFile.Close()
+	return result
 }
--- zip.go
+++ zip.go
@@ -11,72 +11,72 @@ import (
 )
 
 func unzip(source, dest string) error {
-  switch runtime.GOOS {
-    case "windows":
-      cmd := exec.Command("powershell", "-Command", "Expand-Archive -LiteralPath", source, "-DestinationPath", dest)
-      cmd.Run()
-    case "linux":
-    if _, err := os.Stat("/bin/unzip"); err == nil {
-      //fmt.Println("Running unzip")
-      cmd := exec.Command("unzip", source, "-d", dest)
-      cmd.Run()
-      //fmt.Println(cmd.Output())
-    } else {
-      return gounzip(source, dest)
-    }
-  } 
-    
-  return nil
+	switch runtime.GOOS {
+	case "windows":
+		cmd := exec.Command("tar", "-xf", source, "-C", dest)
+		cmd.Run()
+	case "linux":
+		if _, err := os.Stat("/bin/unzip"); err == nil {
+			//fmt.Println("Running unzip")
+			cmd := exec.Command("unzip", source, "-d", dest)
+			cmd.Run()
+			//fmt.Println(cmd.Output())
+		} else {
+			return gounzip(source, dest)
+		}
+	}
+
+	return nil
 }
 
 func gounzip(source, dest string) error {
-  fmt.Println("unzip not found, using Go unzip which will NOT extract overrides")
-    r, err := zip.OpenReader(source)
-    if err != nil {
-        return fmt.Errorf("failed to open ZIP file: %w", err)
-    }
-    defer r.Close()
+	fmt.Println("unzip not found, using Go unzip which will NOT extract overrides")
+	r, err := zip.OpenReader(source)
+	if err != nil {
+		return fmt.Errorf("failed to open ZIP file: %w", err)
+	}
+	defer r.Close()
 
-    if err := os.MkdirAll(dest, 0755); err != nil {
-        return fmt.Errorf("failed to create destination directory: %w", err)
-    }
+	if err := os.MkdirAll(dest, 0755); err != nil {
+		return fmt.Errorf("failed to create destination directory: %w", err)
+	}
 
-    for _, f := range r.File {
-        rc, err := f.Open()
-        if err != nil {
-            return fmt.Errorf("failed to open file in ZIP: %w", err)
-        }
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return fmt.Errorf("failed to open file in ZIP: %w", err)
+		}
 
-        fPath := filepath.Join(dest, f.Name)
+		fPath := filepath.Join(dest, f.Name)
 
-        if f.FileInfo().IsDir() {
-            if err := os.MkdirAll(fPath, f.Mode()); err != nil {
-                rc.Close()
-                return fmt.Errorf("failed to create directory: %w", err)
-            }
-            rc.Close()
-            continue
-        }
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(fPath, f.Mode()); err != nil {
+				rc.Close()
+				return fmt.Errorf("failed to create directory: %w", err)
+			}
+			rc.Close()
+			continue
+		}
 
-        if err := os.MkdirAll(filepath.Dir(fPath), 0755); err != nil {
-            rc.Close()
-            return fmt.Errorf("failed to create directory for file: %w", err)
-        }
+		if err := os.MkdirAll(filepath.Dir(fPath), 0755); err != nil {
+			rc.Close()
+			return fmt.Errorf("failed to create directory for file: %w", err)
+		}
 
-        outFile, err := os.Create(fPath)
-        if err != nil {
-            rc.Close()
-            return fmt.Errorf("failed to create file: %w", err)
-        }
+		outFile, err := os.Create(fPath)
+		if err != nil {
+			rc.Close()
+			return fmt.Errorf("failed to create file: %w", err)
+		}
 
-        if _, err := io.Copy(outFile, rc); err != nil {
-            outFile.Close()
-            rc.Close()
-            return fmt.Errorf("failed to copy file contents: %w", err)
-        }
+		if _, err := io.Copy(outFile, rc); err != nil {
+			outFile.Close()
+			rc.Close()
+			return fmt.Errorf("failed to copy file contents: %w", err)
+		}
 
-        outFile.Close()
-        rc.Close()
-      }
-      return nil
-  }
+		outFile.Close()
+		rc.Close()
+	}
+	return nil
+}
